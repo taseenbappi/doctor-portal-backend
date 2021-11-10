5fa08f0de08a7bb7c5de9e95125077eeca31b490
taseenbappi <taseen.bappi@gmail.com>
2021-11-10 22:21:12 +0600

    verify token added

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// firebase admin
+// doctors-portal-adminsdk.json
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./doctors-portal-adminsdk.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization.startWith("Bearer ")) {
+        const token = req.headers.authorization.split(' ')[1]
+        try {
+            const decoderEmail = await admin.auth().verifyIdToken(token);
+            req.decoderEmail = decoderUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wyglv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -37,6 +64,19 @@ async function run() {
             res.json(result);
         })
 
+        //admin check api
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.roll === "admin") {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+
+        })
+
         //insert appoinments to doctorDB
         app.post('/appointments', async (req, res) => {
             const appoinment = req.body;
@@ -64,6 +104,28 @@ async function run() {
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
+        })
+        //update user roll
+        app.put('/user/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const requester = req.decoderEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.roll === "admin") {
+                    const filter = { email: user.email };
+                    const updateDoc = {
+                        $set: { roll: "admin" }
+                    };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+                else {
+                    res.status(401).json({ message: "Your don't have permission" });
+                }
+
+            }
+
         })
     }
     finally {
